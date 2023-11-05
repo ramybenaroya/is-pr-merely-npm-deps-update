eba9e31646fe421abcc7b1f8087c13f0ba3290b0
Ramy Ben Aroya <ramybenaroya@gmail.com>
2023-11-05 13:11:26 +0200

    force install in working dir

--- install.js
+++ install.js
@@ -1,6 +1,10 @@
 const { exec } = require('child_process');
+const path = require('path');
 
-exec('npm ci', (error, stdout, stderr) => {
+// Get the directory of the current JS file
+const currentDir = path.dirname(__filename);
+
+exec('npm ci', { cwd: currentDir }, (error, stdout, stderr) => {
     if (error) {
         console.error(`exec error: ${error}`);
         return;
